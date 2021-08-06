40e5eb3953c5b3f4912061518ce651ff3a2c2c18
heyseppy <sepkimiaei1@gmail.com>
2021-08-06 13:07:36 +0800

    created directory structures

---
+++ film-club/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import {AppBar, Toolbar, Typography} from '@material-ui/core';
+
+function App() {
+  return (
+    <AppBar position='static'>
+    <Toolbar>
+        <Typography>film club admin portal</Typography>
+    </Toolbar>
+
+ </AppBar>
+        
+  );
+}
+
+export default App;
